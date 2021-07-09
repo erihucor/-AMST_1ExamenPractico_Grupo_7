bd8e055f79c065090a6affd16da33dbce7b483bb
ericksitocg <ericksitocg@hotmail.com>
2021-07-09 18:09:09 -0500

    LlenarListView con lista de strings

--- app/src/main/java/com/example/amst_buscador_de_heroes/SearchResults.java
+++ app/src/main/java/com/example/amst_buscador_de_heroes/SearchResults.java
@@ -6,8 +6,22 @@ import android.os.Bundle;
 
 import java.util.ArrayList;
 import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class SearchResults extends AppCompatActivity {
+
     protected ArrayList<String> searchResults = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +29,26 @@ public class SearchResults extends AppCompatActivity {
         setContentView(R.layout.activity_search_results);
         Intent searchResultsView = getIntent();
         this.searchResults = (ArrayList<String>) searchResultsView.getExtras().get("results");
-        System.out.println(searchResults.toString());
+        //System.out.println("Resultado: " + searchResults.toString());
+        llenarListView();
+
     }
+
+    protected void llenarListView(){
+        ListView list_heroes = (ListView) findViewById(R.id.list_view_heroes);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,this.searchResults);
+        list_heroes.setAdapter(adapter);
+
+        list_heroes.setClickable(true);
+        list_heroes.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        System.out.println("Click");
+                    }
+                }
+
+        );
+    }
+
 }
