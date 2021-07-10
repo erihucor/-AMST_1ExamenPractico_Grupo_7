5782aeffb265b8d9daf1a54de8ca7cf66782a7b7
ericksitocg <ericksitocg@hotmail.com>
2021-07-10 01:38:42 -0500

    add: buscar_heroe(), llenar lista de nombres y diccionario de ID, Fix: llamar a presentar lista

--- app/src/main/java/com/example/amst_buscador_de_heroes/SearchResults.java
+++ app/src/main/java/com/example/amst_buscador_de_heroes/SearchResults.java
@@ -5,43 +5,101 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class SearchResults extends AppCompatActivity {
 
-    protected ArrayList<Map<String,String>> searchResults = new ArrayList<>();
+    private String superhero = "";
+    final String tokenAPI = "4159910954087857";
+    private ArrayList<String> searchResults;
+    private Map<String,String> nombreToid;
+    private RequestQueue mQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+        mQueue = Volley.newRequestQueue(this);
+
         Intent searchResultsView = getIntent();
+        this.superhero = (String) searchResultsView.getExtras().get("superhero");
 
-        this.searchResults = (ArrayList<Map<String, String>>) searchResultsView.getExtras().get("results");
+        searchResults = new ArrayList<>();
+        nombreToid = new HashMap<>();
 
-        ArrayList<String> response_nombres = new ArrayList<>();
-        for(int i = 0; i < response_nombres.size();i++){
-            System.out.println(searchResults.get(i));
-        }
-
-        //System.out.println("Resultado: " + searchResults.toString());
-       // llenarListView();
-
+        buscarHeroe(this.superhero);
     }
 
-    protected void llenarListView(){
+    protected void llenarArrayNombres(JSONArray resultsJSON) throws JSONException {
+        for (int i = 0; i < resultsJSON.length(); i++)
+        {
+            String nombre = resultsJSON.getJSONObject(i).getString("name");
+            String id = resultsJSON.getJSONObject(i).getString("id");
+            this.searchResults.add(nombre);
+            this.nombreToid.put(nombre,id);
+        }
+        presentarListaNombres(this.searchResults,this.nombreToid);
+    }
+
+    protected void presentarListaNombres(ArrayList<String> searchResults, Map<String,String> nombreToid){
         ListView list_heroes = (ListView) findViewById(R.id.list_view_heroes);
-        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,this.searchResults);
-        //list_heroes.setAdapter(adapter);
+        TextView total_resultados = (TextView) findViewById(R.id.text_count_results);
+        total_resultados.setText(Integer.toString(searchResults.size()));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,searchResults);
+        list_heroes.setAdapter(adapter);
 
         list_heroes.setClickable(true);
-        list_heroes.setOnItemClickListener(
-                (adapterView, view, i, l) -> System.out.println("Click")
 
-        );
+        list_heroes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                String nombre = (String) parent.getItemAtPosition(position);
+                String ID = nombreToid.get(nombre);
+                toView_HeroeProfile(ID);
+                }
+            });
+        }
+
+    public void buscarHeroe(String superhero){
+        String url = "https://superheroapi.com/api/"+tokenAPI+"/search/"+superhero;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
+                    //System.out.println(response);
+                    try {
+                        JSONArray results = response.getJSONArray("results");
+                        llenarArrayNombres(results);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }, error -> {
+                    // TODO: Handle error
+
+                });
+        mQueue.add(jsonObjectRequest);
     }
 
+    public void toView_HeroeProfile(String ID) {
+        Intent heroe_profile = new Intent(getBaseContext(),HeroeProfile.class);
+        heroe_profile.putExtra("id", ID);
+        startActivity(heroe_profile);
+    }
 }
